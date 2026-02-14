d46b464165ed3fee00418b84e1b152aa5d8c9faa
NIKHIL RATRA <nikhilratra91@gmail.com>
2026-02-14 09:22:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ export default function Page() {
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
           <div className="my-4 text-4xl font-bold">
-            WOOOOOO!!! I love you pookie!! ;))
+            WOOOOOO!!! I love you pocoooo🦋🦋🦋!! ;))
           </div>
         </>
       ) : (
